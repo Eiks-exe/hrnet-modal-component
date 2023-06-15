371c3695fddf6973518c429c3278a53671556c74
Eiks-exe <eiks_art@outlook.com>
2023-06-15 12:08:02 -0400

    init

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface ModalProps {
+    
+}
+
+const Modal: React.FC<ModalProps> = (props: ModalProps) =>{
+    return (
+        <>
+            modal
+        </>
+    )
+}
+
+export default Modal
