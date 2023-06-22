0e2de97254f11245935239e70e9c76a3a2408760
Eiks-exe <eiks_art@outlook.com>
2023-06-22 15:56:15 -0400

    added css plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
 
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
@@ -30,12 +31,13 @@ export default [
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
             terser(),
+            postcss()
         ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts.default()],
-        external: [],
+        external: [/\.css$/],
     },
 ];
