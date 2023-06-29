efde8d600e2cc2a46701ee5c87dec77233442de0
Eiks-exe <eiks_art@outlook.com>
2023-06-29 11:40:19 -0400

    Readme.md

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# hrnet-modal-component
+# hrnet-modal-component:
 
 modal component for hr-net
+
+## installation : 
+
+using npm : 
+
+``npm install @eiks-exe/hrnet-modal-component@0.2.0``
+
+via package.json:
+
+``"@eiks-exe/hrnet-modal-component": "0.2.0"``
+
+add the component in your project : 
+
+```javascript
+    <Modal content='Employee Created
+                ' isOpen={isOpen} 
+                onClick={() => { setIsOpen(false) }} 
+    >
+    </Modal>
+```
+## properties :
+
+`content`: set the content of the modal
+
+`isOpen`: verify the the state of the modal (opened or closed)
+
+`onClick`: take a function that launches when the user click on a close or return button
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,23 +1,59 @@
-import React from 'react'
-import "./Modal.css"
-type Props = {
-    setIsOpen: Function ;
-    content?: string;
-    border?: "#000" | "#00f"
+import React from 'react';
+import './Modal.css';
+
+interface Props {
+  children?: React.ReactNode;
+  className?: string;
+  onClick?: () => void
+}
+interface PropsModal extends Props {
+  /** Change the modal State */
+  isOpen?: boolean;
+  /** Define the modal header title text */
+  content?: string;
+  /** CSS className of modal container */
+  className?: string;
+  /** CSS classNames of modal elements */
+  style?: {
+    crossClassName?: string
+    buttonClassName?: string
+  }
 }
 
-const Modal = ({setIsOpen, content, border}: Props) => {
-    return (
-        <div className='modal-body'>
-            <div className="modal-container">
-                <div className="modal-content">
-                    {content}
-                </div>
-                <button className="closeBtn" onClick={() => setIsOpen(false)}>x</button>
-                <button className="returnBtn" onClick={() => setIsOpen(false)}>go back</button>
-            </div>
+
+
+const Modal: React.FC<PropsModal> = ({ isOpen, content, onClick, className, style }) => {
+
+  const modalRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    window.onclick = function (event) {
+      if (modalRef && modalRef?.current && event.target == modalRef?.current) {
+        onClick && onClick();
+      }
+    };
+
+    return () => {
+      window.onclick = null;
+    };
+  }, []);
+
+  return (
+    <div className={'modal-body' + ` ${className || ''}`}
+      ref={modalRef}
+      style={{ display: isOpen ? 'flex' : 'none' }}
+    >
+      <div className="modal-container">
+        <div className="close-container">
+          <button className={"closeBtn" + ` ${style?.crossClassName || ''}`} onClick={onClick}>x</button>
         </div>
-    )
+        <div className="modal-content">
+          {content}
+        </div>
+        <button className={"returnBtn" + ` ${style?.buttonClassName || ''}`} onClick={onClick}>go back</button>
+      </div>
+    </div>
+  )
 }
 
 export default Modal
